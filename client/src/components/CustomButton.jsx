36e14a8d5ce5dae5b8bf71bcec712c3b0d882980
stino-x <107426247+stino-x@users.noreply.github.com>
2024-07-25 15:15:58 +0200

    Home page animate presence animation

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
+const CustomButton = ({title, customStyles, handleClick, type}) => {
+    const snap  = useSnapshot(state)
+    const generateStyle = (type) => {
+        switch (type) {
+            case 'filled':
+                return {backgroundColor: snap.color, color: '#fff'}
+            case 'outlined':
+                return 'border border-black text-black'
+            default:
+                return 'bg-black text-white'
+        }
+    }
+  return (
+    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)} onClick={handleClick}>{ title }</button>
+  )
+}
+
+CustomButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  customStyles: PropTypes.string,
+  handleClick: PropTypes.func,
+  type: PropTypes.string
+};
+
+export default CustomButton
---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,6 @@
+
+export default function Customizer() {
+  return (
+    <div>Customizer</div>
+  )
+}
